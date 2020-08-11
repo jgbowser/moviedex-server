666f3aa82b64943a96fe4ca15ffa2f6baaf241fe
John Bowser <john.bow7720@gmail.com>
2020-08-11 13:34:01 -0600

    added genre filtering functionalit

--- moviedexServer.js
+++ moviedexServer.js
@@ -3,11 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const movieData = require('./movie-data.js');
+const movieData = require('./movie-data.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(validateBearerToken);
+
+// Search Handler/Utility functions
 
 function validateBearerToken(req, res, next) {
   const API_TOKEN = process.env.API_TOKEN;
@@ -19,10 +22,20 @@ function validateBearerToken(req, res, next) {
   next();
 }
 
-app.use(validateBearerToken);
+function handleGenreSearch(req, res, next) {
+  const { genre } = req.query;
+  
+  if(genre) {
+    const genreLower = genre.toLowerCase();
+    let movies = [...movieData.movies];
+    movies = movies.filter(movie => movie.genre.toLowerCase().includes(genreLower));
+    return res.status(200).json(movies);
+  }
+  next();
+}
 
-app.get('/movie', (req, res) => {
-  res.send('Working');
-});
+
+//API Request/Response handler
+app.get('/movie', handleGenreSearch, (req, res) => res.send('Hello'));
 
 app.listen(8000, () => console.log('Listening on PORT 8000'));
